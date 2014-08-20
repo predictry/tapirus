7af93757301efa4a8b8d5ff2c18f3325f92737a4
Guilherme <guilherme.dinisjunior@gmail.com>
2014-08-20 21:43:53 +0800

    amended error messages

--- src/predictry/api/v1/errors.py
+++ src/predictry/api/v1/errors.py
@@ -12,18 +12,18 @@ def error(err, object=None, property=None):
 
     errors = {
         'ResourceDoesNotExist': dict(error="Resource " + ent(object) + "not found",
-                                     message="Resource " + ent(object) + "with given properties not exist", status=404),
+                                     message="Resource " + ent(object) + "with given properties does not exist.", status=404),
         'ResourceIdNotProvided': dict(error="Resource " + ent(object) + "{id} property not provided",
-                                      message="Resource " + ent(object) + "{id} property must be specified",
+                                      message="Resource " + ent(object) + "{id} property must be specified.",
                                       status=400),
         'ResourceAlreadyExists': dict(error="Resource already exists",
-                                      message="Resource " + ent(object) + "with that ID already exists", status=400),
-        'UndefinedParameter': dict(error="Undefined parameter value",
-                                   message="The parameter " + ent(property) + "must have a value", status=400),
+                                      message="Resource " + ent(object) + "with that ID already exists.", status=400),
+        'UndefinedParameter': dict(error="Undefined parameter value.",
+                                   message="The parameter " + ent(property) + "must have a value.", status=400),
         'MissingParameter': dict(error="Missing parameter",
-                                       message="The parameter " + ent(property) + "is missing from your request", status=400),
+                                       message="The parameter " + ent(property) + "is missing from your request.", status=400),
         'Unknown': dict(error="An unknown error has occurred",
-                        message="For unknown reasons, the transaction has failed", status=500)
+                        message="For unknown reasons, the transaction has failed.", status=500)
 
     }
 
