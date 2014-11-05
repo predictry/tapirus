af50d189a50c6fbbf2e4f552dd23935a23e0fa94
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2014-11-05 15:36:54 +0800

    fix config loading with missing config file

--- src/predictry/utils/config.py
+++ src/predictry/utils/config.py
@@ -2,7 +2,6 @@ __author__ = 'guilherme'
 
 import os
 import json
-#from predictry.utils.log.logger import Logger
 
 PROJECT_BASE = ''.join([os.path.dirname(os.path.abspath(__file__)), "/../"])
 CONFIG_FILE = ''.join([PROJECT_BASE, 'server-config.json'])
@@ -11,14 +10,17 @@ config = dict()
 
 
 def load_configuration():
-    f = open(CONFIG_FILE, 'r')
 
-    contents = ''.join(f.readlines())
+    if os.path.isfile(CONFIG_FILE):
 
-    global config
-    config = json.loads(contents)
+        f = open(CONFIG_FILE, 'r')
 
-    if "log_config_file_name" in config:
-        config["log_config_file"] = ''.join([PROJECT_BASE, config["log_config_file_name"]])
+        contents = ''.join(f.readlines())
+
+        global config
+        config = json.loads(contents)
+
+        if "log_config_file_name" in config:
+            config["log_config_file"] = ''.join([PROJECT_BASE, config["log_config_file_name"]])
 
 load_configuration()
