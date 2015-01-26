4c4a983714ee733bb4946c7b1db833497597db94
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-01-26 20:31:59 +0800

    Greetings endpoint. Closes #10

---
+++ src/tapirus/server.py
@@ -0,0 +1,20 @@
+__author__ = 'guilherme'
+
+
+from flask import Flask, Response, url_for, request
+import json
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+
+    data = {
+        "message": "{0}".format("Greetings")
+    }
+
+    return Response(json.dumps(data), status=200, mimetype="application/json")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
