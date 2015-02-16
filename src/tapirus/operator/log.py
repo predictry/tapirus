699e3c3cb9ac20fce6f7a4e3951cd22981c1f04b
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-02-16 19:59:09 +0800

    Refactor log processing to its own operator. Closes #51

---
+++ src/tapirus/operator/log.py
@@ -0,0 +1,118 @@
+__author__ = 'guilherme'
+
+
+import gzip
+import traceback
+
+from tapirus.utils import jsonuri
+from tapirus.operator import schema
+from tapirus.utils.logger import Logger
+
+
+LOG_FILE_COLUMN_SEPARATOR = "\t"
+
+
+def process_log(file_name, batch_size, processor):
+    """
+
+    :param file_name:
+    :return:
+    """
+
+    queries = []
+
+    with gzip.open(file_name, 'rb') as f:
+        """
+            #indeces
+            #0: date
+            #1: time
+            #4: ip
+            #5: method
+            #6: server
+            #7: path
+            #8: status
+            #9: source page
+            #10: agent (encoded)
+            #11: payload
+        """
+
+        count = 0
+
+        for line in f:
+
+            l = line.decode(encoding="utf-8").split(LOG_FILE_COLUMN_SEPARATOR)
+
+            if len(l) >= 12:
+
+                date, time, ip, path, status = l[0], l[1], l[4], l[7], int(l[8])
+
+                if ".gif" not in path or status not in [0, 200, 304]:
+                    continue
+
+                try:
+
+                    payload = jsonuri.deserialize(l[11])
+
+                except ValueError as e:
+
+                    Logger.error("Error in deserialization of payload: [{0}]\n\t{1}".format(l[11], e))
+
+                    continue
+
+                queries.extend(schema.generate_queries(date, time, ip, path, payload))
+                count += 1
+
+            if count % batch_size == 0 and count > 0:
+
+                #upload
+                #run queries
+                try:
+                    #rs = neo4j.run_batch_query(queries, commit=True)
+                    processor(queries)
+
+                except Exception as e:
+                    Logger.error(traceback.format_exc())
+                    raise e
+                else:
+
+                    print("[Processed {0} actions {{Total: {1}}}, with {2} queries]".format(
+                        (count//batch_size + 1)*batch_size - count,
+                        count,
+                        len(queries))
+                    )
+
+                    Logger.info("[Processed {0} actions {{Total: {1}}}, with {2} queries]".format(
+                        (count//batch_size + 1)*batch_size - count,
+                        count,
+                        len(queries))
+                    )
+
+                    queries.clear()
+
+        if queries:
+
+            try:
+
+                #rs = neo4j.run_batch_query(queries, commit=True)
+                processor(queries)
+
+            except Exception as e:
+                Logger.error(traceback.format_exc())
+                raise e
+
+            else:
+                print("[Processed {0} actions {{Total: {1}}}, with {2} queries.".format(
+                    (count//batch_size + 1)*batch_size - count,
+                    count,
+                    len(queries))
+                )
+
+                Logger.info("[Processed {0} actions {{Total: {1}}}, with {2} queries.".format(
+                    (count//batch_size + 1)*batch_size - count,
+                    count,
+                    len(queries))
+                )
+
+                queries.clear()
+
+    Logger.info("Processed [`{0}`] records in log file [`{1}`]".format(count, file_name.split("/")[-1]))
--- src/tapirus/workers/harvester.py
+++ src/tapirus/workers/harvester.py
@@ -14,31 +14,10 @@ from tapirus.utils import jsonuri
 from tapirus.utils import config
 from tapirus.core.db import neo4j
 from tapirus.core import aws
-from tapirus.operator import schema
+from tapirus.operator import log
 from tapirus.operator import log_keeper
 from tapirus.utils.logger import Logger
 from tapirus.utils import io
-
-LOG_FILE_COLUMN_SEPARATOR = "\t"
-
-
-SESSION_ID = "session_id"
-TENANT_ID = "tenant_id"
-ITEM_ID = "item_id"
-USER_ID = "user_id"
-AGENT_ID = "browser_id"
-USER = "user"
-AGENT = "browser"
-ACTION = "action"
-NAME = "name"
-QUANTITY = "qty"
-TOTAL = "total"
-SUB_TOTAL = "sub_total"
-ITEMS = "items"
-KEYWORDS = "keywords"
-LOCATION = "locations"
-CITY = "city"
-COUNTRY = "country"
 
 
 def get_file_from_queue():
@@ -73,132 +52,9 @@ def get_file_from_queue():
         return None, None
 
 
-def process_log(file_name, batch_size):
-    """
+def processor(queries):
 
-    :param file_name:
-    :return:
-    """
-
-    queries = []
-
-    with gzip.open(file_name, 'rb') as f:
-        """
-            #indeces
-            #0: date
-            #1: time
-            #4: ip
-            #5: method
-            #6: server
-            #7: path
-            #8: status
-            #9: source page
-            #10: agent (encoded)
-            #11: payload
-        """
-
-        count = 0
-
-        for line in f:
-
-            l = line.decode(encoding="utf-8").split(LOG_FILE_COLUMN_SEPARATOR)
-
-            if len(l) >= 12:
-
-                date, time, ip, path, status = l[0], l[1], l[4], l[7], int(l[8])
-
-                if ".gif" not in path or status not in [0, 200, 304]:
-                    continue
-
-                try:
-
-                    payload = jsonuri.deserialize(l[11])
-
-                except ValueError as e:
-
-                    Logger.error("Error in deserialization of payload: [{0}]\n\t{1}".format(l[11], e))
-
-                    continue
-
-                queries.extend(schema.generate_queries(date, time, ip, path, payload))
-                count += 1
-
-            if count % batch_size == 0 and count > 0:
-
-                #upload
-                #run queries
-                try:
-                    rs = neo4j.run_batch_query(queries, commit=True)
-
-                except Exception as e:
-                    Logger.error(traceback.format_exc())
-                    raise e
-                else:
-
-                    print("[Processed {0} actions {{Total: {1}}}, with {2} queries]".format(
-                        (count//batch_size + 1)*batch_size - count,
-                        count,
-                        len(queries))
-                    )
-
-                    Logger.info("[Processed {0} actions {{Total: {1}}}, with {2} queries]".format(
-                        (count//batch_size + 1)*batch_size - count,
-                        count,
-                        len(queries))
-                    )
-
-                    queries.clear()
-
-        if queries:
-            try:
-
-                rs = neo4j.run_batch_query(queries, commit=True)
-
-            except Exception as e:
-                Logger.error(traceback.format_exc())
-                raise e
-
-            else:
-                print("[Processed {0} actions {{Total: {1}}}, with {2} queries.".format(
-                    (count//batch_size + 1)*batch_size - count,
-                    count,
-                    len(queries))
-                )
-
-                Logger.info("[Processed {0} actions {{Total: {1}}}, with {2} queries.".format(
-                    (count//batch_size + 1)*batch_size - count,
-                    count,
-                    len(queries))
-                )
-
-                queries.clear()
-
-    Logger.info("Processed [`{0}`] records in log file [`{1}`]".format(count, file_name.split("/")[-1]))
-
-
-def is_acceptable_data_type(e):
-    """
-
-    :param e:
-    :return:
-    """
-
-    #todo: remove func
-
-    if type(e) in [bool, int, float, complex, str, bytes, list, set]:
-
-        if type(e) is list or set:
-
-            for elem in e:
-                if type(elem) is dict:
-                    return False
-        else:
-            return True
-
-    else:
-        return False
-
-    return True
+    rs = neo4j.run_batch_query(queries, commit=True)
 
 
 def run():
@@ -237,7 +93,7 @@ def run():
         aws.download_log_from_s3(s3_file_path, file_path)
 
         #Process log
-        process_log(file_path, batch_size)
+        log.process_log(file_path, batch_size, processor)
 
         #Delete downloaded file
         io.delete_file(file_path)
