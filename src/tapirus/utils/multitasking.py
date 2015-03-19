1ad4508d92b18082b0d3d3851eebe554eb126afc
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-03-19 16:59:14 +0800

    Added optional wait option for repeat func calls. Closes #79

--- src/tapirus/utils/multitasking.py
+++ src/tapirus/utils/multitasking.py
@@ -4,9 +4,14 @@ import threading
 import multiprocessing.context
 
 
-def repeat(interval, worker_function, args=(), iterations=0):
-    if iterations != 1:
-        threading.Timer(interval, repeat, [interval, worker_function, args, 0 if iterations == 0 else iterations-1]).start()
+def repeat(interval, worker_function, args=(), wait=False, iterations=0):
 
     job = multiprocessing.context.Process(target=worker_function, args=args)
     job.start()
+
+    if wait:
+        job.join()
+
+    if iterations != 1:
+        thread = threading.Timer(interval, repeat, [interval, worker_function, args, 0 if iterations == 0 else iterations-1])
+        thread.start()
