0d0d4ff7e66216059f24cc3b5e43ea293ef582da
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-03-19 16:46:18 +0800

    Added support for args in repeated func calls. Closes #80

--- src/tapirus/utils/multitasking.py
+++ src/tapirus/utils/multitasking.py
@@ -4,9 +4,9 @@ import threading
 import multiprocessing.context
 
 
-def repeat(interval, worker_function, iterations=0):
+def repeat(interval, worker_function, args=(), iterations=0):
     if iterations != 1:
-        threading.Timer(interval, repeat, [interval, worker_function, 0 if iterations == 0 else iterations-1]).start()
+        threading.Timer(interval, repeat, [interval, worker_function, args, 0 if iterations == 0 else iterations-1]).start()
 
-    job = multiprocessing.context.Process(target=worker_function)
+    job = multiprocessing.context.Process(target=worker_function, args=args)
     job.start()
