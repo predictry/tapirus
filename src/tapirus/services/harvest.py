14656547752bba89f94548243ee71facfb5d9dae
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-06-11 13:30:15 +0800

    fixed #111. New record state: NOT FOUND

--- src/tapirus/services/harvest.py
+++ src/tapirus/services/harvest.py
@@ -1,31 +1,22 @@
 import os
 import os.path
-import errno
 import tempfile
-import shutil
-import csv
-import json
-import time
+import datetime
 
 import luigi
 import luigi.file
 
-if os.name == 'posix':
-    tempfile.tempdir = "/tmp"
-else:
-    tempfile.tempdir = "out"
-
-
-import luigi
-import luigi.file
-
+from tapirus import constants
 from tapirus.core import aws
 from tapirus.dao import RecordDAO, LogFileDAO
 from tapirus.entities import Record, LogFile
-from tapirus.core import errors
-from tapirus import constants
 from tapirus.utils import config
 from tapirus.utils.logger import Logger
+
+if os.name == 'posix':
+    tempfile.tempdir = '/tmp'
+else:
+    tempfile.tempdir = 'out'
 
 
 class DownloadedRecordTarget(luigi.Target):
@@ -36,16 +27,26 @@ class DownloadedRecordTarget(luigi.Target):
 
     def exists(self):
 
-        if RecordDAO.exists(self.date, self.hour):
+        timestamp = datetime.datetime(
+            year=self.date.year,
+            month=self.date.month,
+            day=self.date.day,
+            hour=self.hour
+        )
 
-            record = RecordDAO.read(self.date, self.hour)
+        if RecordDAO.exists(timestamp=timestamp):
 
-            if record.status == constants.STATUS_PENDING:
+            record = RecordDAO.read(timestamp=timestamp)
+
+            if record.status in (constants.STATUS_PENDING, constants.STATUS_NOT_FOUND):
                 return False
 
         else:
 
-            record = Record(None, self.date, self.hour, None, constants.STATUS_PENDING, None)
+            record = Record(id=None, timestamp=timestamp, last_updated=None,
+                            status=constants.STATUS_PENDING,
+                            uri=None
+                            )
 
             _ = RecordDAO.create(record)
 
@@ -66,12 +67,12 @@ class DownloadedRecordTarget(luigi.Target):
 #
 #             record = RecordDAO.read(self.date, self.hour)
 #
-#             if record.status in ("PENDING",):
+#             if record.status in ('PENDING',):
 #                 return False
 #
 #         else:
 #
-#             record = Record(None, self.date, self.hour, None, "PENDING", None)
+#             record = Record(None, self.date, self.hour, None, 'PENDING', None)
 #
 #             _ = RecordDAO.create(record)
 #
@@ -91,6 +92,13 @@ class DownloadLogsTask(luigi.Task):
 
     def run(self):
 
+        timestamp = datetime.datetime(
+            year=self.date.year,
+            month=self.date.month,
+            day=self.date.day,
+            hour=self.hour
+        )
+
         s3 = config.get('s3')
         bucket = s3['bucket']
         prefix = s3['prefix']
@@ -100,23 +108,38 @@ class DownloadLogsTask(luigi.Task):
         keys = aws.S3.list_bucket_keys(bucket, pattern)
         files = []
 
-        record = RecordDAO.read(self.date, self.hour)
+        record = RecordDAO.read(timestamp=timestamp)
 
         for key in keys:
 
             s3_key = '/'.join([bucket, key])
             filename = os.path.join(tempfile.gettempdir(), key)
+
+            if not os.path.exists(os.path.dirname(filename)):
+
+                Logger.debug('Creating directory {0}'.format(os.path.dirname(filename)))
+
+                os.makedirs(os.path.dirname(filename))
+
             files.append(filename)
+
+            Logger.info('Downloading {0} from bucket {1}'.format(key, bucket))
+
             aws.S3.download_file(s3_key, filename)
 
-            logfile = LogFile(None, record.id, filename)
+            logfile = LogFile(id=None, record=record.id, filepath=filename)
 
             _ = LogFileDAO.create(logfile)
 
-        record.status = constants.STATUS_DOWNLOADED
+        if not files or not keys:
+            record.status = constants.STATUS_NOT_FOUND
+            Logger.info('No logs found for timestamp {0}-{1}'.format(self.date, self.hour))
+        elif files:
+            Logger.info('Found {0} logs for timestamp {0}-{1}'.format(self.date, self.hour))
+            record.status = constants.STATUS_DOWNLOADED
 
         assert isinstance(record, Record)
-        r = RecordDAO.update(record)
+        _ = RecordDAO.update(record)
 
 
 class ParseLogs(luigi.Task):
@@ -155,7 +178,7 @@ class ParseLogs(luigi.Task):
 #         #process file
 
 
-# if __name__ == "__main__":
+# if __name__ == '__main__':
 #
 #     print(aws.S3.list_bucket_keys('trackings', pattern='action-logs/ER1VHJSBZAAAA.2015-05-11-11'))
 #
@@ -163,6 +186,6 @@ class ParseLogs(luigi.Task):
 #
 #     print(exists)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     luigi.run()
