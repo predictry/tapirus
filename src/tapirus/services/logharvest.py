1d41b9fefc583bb4d8fd1fc6a0f21dfb609382ac
jocki <jocki.predictry@gmail.com>
2015-11-16 17:48:43 +0800

    Change scheduler start time.

--- src/tapirus/services/logharvest.py
+++ src/tapirus/services/logharvest.py
@@ -1,41 +1,31 @@
 import datetime
-
 import schedule
-
+import time
 from tapirus import tasks
 from tapirus.utils import config
 from tapirus.utils.logger import Logger
 
 
-def harvest_past_hour():
+def harvest():
+    now = datetime.datetime.utcnow()
+    timestamps = [now - datetime.timedelta(hours=x) for x in range(2, 7*24+1)]
 
-    def inner():
-
-        now = datetime.datetime.utcnow()
-        timestamps = [now - datetime.timedelta(hours=x) for x in range(1, 7*24+1)]
-
-        # how do I make sure the timing is right? request for the past two hours
-        for timestamp in timestamps:
-            tasks.run_workflow_for_record.delay(timestamp)
-
-    period = config.get('harvester', 'interval', type=int, fallback=3600)
-    schedule.every(period).seconds.do(inner)
-
-    Logger.info('Initiating log harvesting worker...')
+    # how do I make sure the timing is right? request for the past two hours
+    for timestamp in timestamps:
+        Logger.info('Harvesting for {0}'.format(timestamp))
+        tasks.run_workflow_for_record.delay(timestamp)
 
 
 def main():
-
-    import time
+    Logger.info('Initiating log harvesting worker...')
+    harvest()
+    period = config.get('harvester', 'interval', type=int, fallback=3600)
+    schedule.every(period).seconds.do(harvest)
     while True:
-            schedule.run_pending()
-            time.sleep(1)
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-
-    # register periodic tasks
-    harvest_past_hour()
-
     # run tasks
     main()
